e6ab3b75b508dc93e29dd6e90996bd68203594da
Anthony Reyes <randomhabit@hotmail.com>
2020-05-10 10:58:54 -0500

    MainScript, Choice selection, Random,
    Save TBD

---
+++ iLove-Unity2/Assets/Scripts/mainScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainScript : MonoBehaviour
+{
+    public Sprite[] buttonOptions;
+    public int todaysPiction;
+    private int todaysAnswer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setPicture(int chosenCell)
+    {
+        todaysPiction = chosenCell;
+    }
+}
---
+++ iLove-Unity2/Assets/choiceScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class choiceScript : MonoBehaviour
+{
+    private int cellNumber;
+    private int maxSize = 1;
+    private Image mine;
+    Button butt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        butt = GetComponent<Button>();
+        butt.onClick.AddListener(madeSelection);
+        mine = GetComponent<Image>();
+        maxSize = GameObject.Find("startDetector").GetComponent<mainScript>().buttonOptions.Length;
+        cellNumber = Random.Range(0, maxSize);
+        //UnityEngine.Debug.Log(cellNumber);
+        mine.sprite = GameObject.Find("startDetector").GetComponent<mainScript>().buttonOptions[cellNumber];
+    }
+
+    void madeSelection()
+    {
+        UnityEngine.Debug.Log("hey you clicked me");
+        GameObject.Find("startDetector").GetComponent<mainScript>().setPicture(cellNumber);
+        GameObject results = GameObject.Find("ResultCanvas");
+        results.SetActive(true);
+        GameObject choicesScr = GameObject.Find("choiceCanvas");
+        choicesScr.SetActive(false);
+    }
+}
